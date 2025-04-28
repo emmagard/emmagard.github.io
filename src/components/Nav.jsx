e85339d2435cba42b0e8d03a45ec31b5bfb527b0
Emma Gardner <emma@threadablebooks.com>
2025-04-28 13:24:36 -0400

    Update resume

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useLocation } from 'react-router-dom';
 import { getLcn } from '../utils/labelled-classnames';
 import NavItem from './NavItem';
